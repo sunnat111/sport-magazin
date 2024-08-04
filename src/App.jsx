128e16ff62771139610aa12555157a08a75ee29a
dff <dff@.ru>
2024-08-04 20:53:27 +0300

    sport

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './scss/main.scss'
+import Slider from './components/Slider/Slider'
+import Mainpage from './components/Mainpage/Mainpage'
+
+function App() {
+
+  return (
+    <>
+      <Mainpage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/imgExport.jsx
@@ -0,0 +1,11 @@
+export { default as swiperimage } from "../assets/images/swiperimage.jpeg";
+export { default as image1 } from "../assets/images/image1.png";
+export { default as image2 } from "../assets/images/image2.png";
+export { default as image3 } from "../assets/images/image3.png";
+export { default as image4 } from "../assets/images/image4.png";
+export { default as image5 } from "../assets/images/image5.png";
+export { default as image6 } from "../assets/images/image6.png";
+export { default as image7 } from "../assets/images/image7.png";
+export { default as image8 } from "../assets/images/image8.png";
+export { default as image9 } from "../assets/images/image9.png";
+export { default as image10 } from "../assets/images/image10.png";
